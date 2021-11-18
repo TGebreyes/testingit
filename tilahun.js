116aa479e707e2624ace2d6a9e9d44fff30e3326
tilahunbogale <tilahunbogale@gmail.com>
2021-11-18 10:32:54 -0500

    3rd

---
+++ tilahun.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.student = void 0;
+var student = /** @class */ (function () {
+    function student() {
+    }
+    return student;
+}());
+exports.student = student;
+//# sourceMappingURL=tilahun.js.map
