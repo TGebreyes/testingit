3279f95b03717b2d87acef9baa74ecf5fb7c83ba
tilahunbogale <tilahunbogale@gmail.com>
2021-11-18 09:43:50 -0500

    added

---
+++ newFile.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.user = void 0;
+var user = /** @class */ (function () {
+    function user() {
+    }
+    return user;
+}());
+exports.user = user;
+//# sourceMappingURL=newFile.js.map
